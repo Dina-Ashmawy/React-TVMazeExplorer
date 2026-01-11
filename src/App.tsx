fe868d1fd3a8e05534d0f665111993139064fd29
xische_dhashmawy <xische_dhashmawy@dm.gov.ae>
2026-01-11 21:56:10 +0400

    Fix readme and update packages versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SeasonsPage from './pages/seasonPage';
 import HomePage from './pages/homePage';
